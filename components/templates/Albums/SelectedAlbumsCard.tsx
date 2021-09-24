ac89ed38720e2774d337e6d07260762c42a83a97
kaito.tada <kuyt1992@gmail.com>
2021-09-24 22:54:35 +0900

    wip: SelectedCardのレイアウト修正

--- components/templates/Albums/SelectedAlbumsCard.tsx
+++ components/templates/Albums/SelectedAlbumsCard.tsx
@@ -10,13 +10,13 @@ export const SelectedAlbumsCard: VFC<Props> = (props) => {
   const { selectedAlbums } = props
 
   return (
-    <div className="bg-gray-800 fixed bottom-0 w-11/12 bg-opacity-90 rounded-md lg:pl-14 mb-1">
-      <div className="flex">
+    <div className="bg-gray-800 fixed bottom-0 w-10/12 bg-opacity-90 rounded-md lg:pl-24 mb-1">
+      <div className="flex flex-wrap">
         {selectedAlbums.map((album) => (
-          <div className="group relative" key={album.id}>
+          <div className="group justify-start items-center h-28 w-28" key={album.id}>
             <img
               key={album.id}
-              className="hover:opacity-60 hover:duration-300 w-1/12 flex lg:p-2 md:p-2 p-1 rounded-sm"
+              className="hover:opacity-60 hover:duration-300 lg:p-2 md:p-2 p-1 pt rounded-sm"
               src={album.images?.[0].url || ''}
               alt={album.name}
             />
